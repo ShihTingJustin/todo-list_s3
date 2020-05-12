2da045354eac1844bee5ae56391af2a4b6cf7fa0
Justin Huang <justinhuang777@gmail.com>
2020-05-12 16:32:55 +0800

    feat: add update todo function

--- app.js
+++ app.js
@@ -59,6 +59,28 @@ app.get('/todos/:id', (req, res) => {
     .catch(error => console.log(error))
 })
 
+app.get('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then(todo => res.render('edit', { todo }))
+    .catch(error => console.log(error))
+})
+
+app.post('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  const name = req.body.name
+  return Todo.findById(id)
+    .then(todo => {
+      todo.name = name
+      return todo.save()
+      // save 是 mongoose 提供的方法
+    })
+    .then(() => res.redirect(`/todos/${id}`))
+    .catch(error => console.log(error))
+})
+
+
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
 })
