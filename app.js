9bbb7a99a3fe647e641b44709bf5a8fc08fd0ce1
Justin Huang <justinhuang777@gmail.com>
2020-05-14 21:47:05 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.get('/', (req, res) => {
   //拿到全部的 Todo 資料
   Todo.find()
     .lean()  //撈資料以後想用 res.render，要先用 .lean 來處理
+    .sort({ _id: 'asc'}) // 資料排序，正序用asc，反序用 desc 
     .then(todos => res.render('index', { todos }))
     .catch(error => console.error(error))
   //可能網路過程發生error，可以把error捕捉記錄下來
