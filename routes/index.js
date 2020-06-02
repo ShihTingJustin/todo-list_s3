01ee4de7a07526da607ac366702c5e360dcf7835
Justin Huang <justinhuang777@gmail.com>
2020-06-02 11:02:32 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router()  //使用express的路由器
 
 const home = require('./modules/home')
 const todos = require('./modules/todos')
+const users = require('./modules/users')
 
 router.use('/', home)
 router.use('/todos', todos)
+router.use('/users', users)
 
 module.exports = router
